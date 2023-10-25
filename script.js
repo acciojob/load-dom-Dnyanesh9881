34f7e4f112d44f97de216e69efe48f870247b1d7
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2023-10-25 16:48:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required
 let body=document.querySelector("body");
-const str=document.creatElement("String");
+const str=document.creatElement("p");
 str.innerText="DOM load Success"
 body.appendChild(str);
