ff23ebe9f5335775cbfc6423a2de9ed6a38887af
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2023-10-25 16:46:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //your JS code here. If required
 let body=document.querySelector("body");
+const str=document.creatElement("String");
+str.innerText="DOM load Success "
  body.addEventListener("load" ,()=>{
 	 Console.log("DOM load Success");
  })
