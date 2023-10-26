cdd642753f0528959938767696d26f6b0a8d97e1
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2023-10-26 15:44:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required
 
 let body=document.querySelector("body");
-body.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", ()=>{
 	// let p=document.createElement("p");
 	// p.innerText="DOM load success"
 	// body.appendChild(p);
